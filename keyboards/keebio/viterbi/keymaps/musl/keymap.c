47bbfdd0f74fe03ccab1e84c4fed6e6416068204
Mike Hix <mhix@newrelic.com>
2019-03-13 17:29:38 -0700

    Add my viterbi keymap

---
+++ keyboards/keebio/viterbi/keymaps/musl/keymap.c
@@ -0,0 +1,135 @@
+#include "viterbi.h"
+#include "action_layer.h"
+#include "eeconfig.h"
+
+extern keymap_config_t keymap_config;
+
+enum my_layers {
+	LAYER_HOME = 0,
+	LAYER_LEFT,
+	LAYER_RIGHT,
+	LAYER_QG,
+	LAYER_SYS,
+	LAYER_COUNT // LAYER_COUNT stays at the end for automatic layer count.
+};
+
+enum my_keycodes {
+	DBL_ZERO = SAFE_RANGE,
+	BL_MODE,
+	WIPE_EEPROM
+};
+
+const uint16_t PROGMEM keymaps[LAYER_COUNT][MATRIX_ROWS][MATRIX_COLS] = {
+
+	/*
+	 * Home: Dvorak Typing
+	 */
+	[LAYER_HOME] = LAYOUT( \
+			KC_GRV,   KC_1,     KC_2,     KC_3,    KC_4,     KC_5,           KC_HOME,          KC_END,          KC_6,           KC_7,        KC_8,     KC_9,     KC_0,     KC_EQL,    \
+			KC_TAB,   KC_QUOT,  KC_COMM,  KC_DOT,  KC_P,     KC_Y,           KC_BSPC,          KC_DEL,          KC_F,           KC_G,        KC_C,     KC_R,     KC_L,     KC_BSLS,   \
+			KC_ESC,   KC_A,     KC_O,     KC_E,    KC_U,     KC_I,           CTL_T(KC_ENT),    CTL_T(KC_ENT),   KC_D,           KC_H,        KC_T,     KC_N,     KC_S,     KC_MINS,   \
+			KC_LSFT,  KC_SCLN,  KC_Q,     KC_J,    KC_K,     KC_X,           ALT_T(KC_PGUP),   ALT_T(KC_PGDN),  KC_B,           KC_M,        KC_W,     KC_V,     KC_Z,     KC_RSFT,   \
+			KC_CAPS,  KC_LEFT,  KC_DOWN,  KC_UP,   KC_RGHT,  GUI_T(KC_SPC),  MO(LAYER_RIGHT),  MO(LAYER_LEFT),  GUI_T(KC_SPC),  RCTL(KC_A),  KC_HYPR,  KC_LBRC,  KC_RBRC,  KC_SLSH \
+			),
+
+	/*
+	 * Left Momentary Layer: F-Keys
+	 */
+	[LAYER_LEFT] = LAYOUT( \
+			KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_TRNS,       TO(LAYER_SYS), KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   \
+			KC_TRNS,  KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_TRNS,       KC_TRNS,       KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   \
+			KC_TRNS,  KC_F11,   KC_F12,   KC_F13,   KC_F14,   KC_F15,   KC_TRNS,       KC_TRNS,       KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   \
+			KC_TRNS,  KC_F16,   KC_F17,   KC_F18,   KC_F19,   KC_F20,   KC_TRNS,       KC_TRNS,       KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   \
+			KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  TO(LAYER_QG),  KC_TRNS,       KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS \
+			),
+
+	/*
+	 * Right Momentary: Media, Mouse, and RGB Controls
+	 */
+	[LAYER_RIGHT] = LAYOUT( \
+			KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  TO(LAYER_SYS),  KC_TRNS,       KC_PAUSE,  KC_PSCR,        KC_WH_U,     KC_SLCK,         BL_MODE,     RGB_TOG,   \
+			KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,        KC_TRNS,       KC_VOLU,   LALT(KC_LEFT),  KC_MS_UP,    LALT(KC_RIGHT),  KC_MS_BTN2,  RGB_VAI,   \
+			KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,        KC_TRNS,       KC_MUTE,   KC_MS_LEFT,     KC_MS_DOWN,  KC_MS_RIGHT,     KC_MS_BTN1,  RGB_VAD,   \
+			KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,        KC_TRNS,       KC_VOLD,   KC_WH_L,        KC_WH_D,     KC_WH_R,         RGB_HUI,     RGB_SAI,   \
+			KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,        TO(LAYER_QG),  KC_TRNS,   KC_PSCR,        KC_HYPR,     KC_SYSREQ,       RGB_HUD,     RGB_SAD \
+			),
+
+	/*
+	 * QWERTY Gaming
+	 */
+	[LAYER_QG] = LAYOUT( \
+			KC_GRV,    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,           KC_TRNS,         KC_TRNS,         KC_NO,          KC_NO,  KC_PSLS,   KC_PAST,  KC_PMNS,  KC_NO,    \
+			KC_TAB,    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,           KC_TRNS,         KC_TRNS,         KC_NO,          KC_P7,  KC_P8,     KC_P9,    KC_PPLS,  KC_NO,    \
+			KC_CAPS,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,           KC_TRNS,         KC_TRNS,         KC_TAB,         KC_P4,  KC_P5,     KC_P6,    KC_PENT,  KC_NO,    \
+			MOD_LSFT,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,           KC_TRNS,         KC_TRNS,         KC_NO,          KC_P1,  KC_P2,     KC_P3,    KC_PENT,  KC_NO,    \
+			KC_LALT,   KC_F1,  KC_F2,  KC_F3,  KC_F4,  GUI_T(KC_SPC),  TO(LAYER_HOME),  TO(LAYER_HOME),  GUI_T(KC_SPC),  KC_P0,  DBL_ZERO,  KC_PDOT,  KC_PCMM,  KC_END \
+			),
+	/*
+	 * System: QMK Functions, Debug, & etc
+	 */
+	[LAYER_SYS] = LAYOUT( \
+			KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  RESET,           RESET,           KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    \
+			KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  EEPROM_RESET,    EEPROM_RESET,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    \
+			KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  DEBUG,           DEBUG,           KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    \
+			KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,           KC_NO,           KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    \
+			KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  TO(LAYER_HOME),  TO(LAYER_HOME),  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO  \
+			)
+				  
+};
+
+
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+	
+	// Store a custom RGB light mode because I want to cycle through a
+	// smaller list of modes.
+	static uint8_t mode = 1;
+
+	switch (keycode) {
+		case DBL_ZERO:
+			if (!record->event.pressed) {
+				SEND_STRING("00");
+			}
+			break;
+		case BL_MODE:
+			if (record->event.pressed) {
+				switch(mode) {
+					case 1:
+						mode = 8;
+						break;
+					case 8:
+						mode = 14;
+						break;
+					default:
+						mode = 1;
+						break;
+				}
+				rgblight_mode(mode);
+			}
+			break;
+	}
+
+	return true;
+}	
+
+void blink(uint8_t times, uint8_t mode, uint8_t r, uint8_t g, uint8_t b) { 
+	rgblight_mode_noeeprom(0);
+	rgblight_setrgb(0, 0, 0);
+	for(uint8_t i = times; --i > 0;) {
+		rgblight_setrgb(r, g, b);
+		_delay_ms(100);
+		rgblight_setrgb(0, 0, 0);
+		_delay_ms(100);
+	}
+	rgblight_mode_noeeprom(mode);
+}
+
+/*
+// This appears to be obsolete as of 2018-11-02.
+void matrix_init_user() {
+	// Manually disable JTAG so it gets done on both halves.
+	MCUCR |= (1<<JTD);       
+	MCUCR |= (1<<JTD);
+}
+*/
+
