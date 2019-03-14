7501f51e3be00ae0214fb6c4aa0e241888246c35
Mike Hix <mhix@newrelic.com>
2019-03-13 18:22:12 -0700

    Slow down the mouse cursor a bit, remove unnecessary rules, be explicit about the RGB_DI_PIN

--- keyboards/keebio/viterbi/keymaps/musl/config.h
+++ keyboards/keebio/viterbi/keymaps/musl/config.h
@@ -70,7 +70,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MOUSEKEY_DELAY             20
 #define MOUSEKEY_INTERVAL          20
 #define MOUSEKEY_MAX_SPEED         10
-#define MOUSEKEY_TIME_TO_MAX       20
+#define MOUSEKEY_TIME_TO_MAX       30
 #define MOUSEKEY_WHEEL_MAX_SPEED   8
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 40
 
@@ -78,8 +78,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // LED Configuration
 /******************************************************************************/
 
+#undef RGB_DI_PIN
+#define RGB_DI_PIN D3
+
 #undef RGBLED_NUM
 #define RGBLED_NUM 2
+
 #define RGBLIGHT_ANIMATIONS
 #define RGBLIGHT_HUE_STEP 16
 #define RGBLIGHT_SAT_STEP 16
