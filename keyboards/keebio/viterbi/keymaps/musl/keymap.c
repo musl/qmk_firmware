0839a2c1d49311297bff458b9ebb186277d5b884
Mike Hix <mhix@newrelic.com>
2019-03-14 17:58:24 -0700

    Clean up, add more F-keys, and more

--- keyboards/keebio/viterbi/keymaps/musl/keymap.c
+++ keyboards/keebio/viterbi/keymaps/musl/keymap.c
@@ -8,8 +8,8 @@ enum my_layers {
 	LAYER_HOME = 0,
 	LAYER_LEFT,
 	LAYER_RIGHT,
-	LAYER_QG,
-	LAYER_SYS,
+	LAYER_PADS,
+	LAYER_SYSTEM,
 	LAYER_COUNT // LAYER_COUNT stays at the end for automatic layer count.
 };
 
@@ -19,65 +19,67 @@ enum my_keycodes {
 	WIPE_EEPROM
 };
 
+#define ____ KC_TRNS
+#define xxxx KC_NO
+
 const uint16_t PROGMEM keymaps[LAYER_COUNT][MATRIX_ROWS][MATRIX_COLS] = {
 
 	/*
 	 * Home: Dvorak Typing
 	 */
 	[LAYER_HOME] = LAYOUT( \
-			KC_GRV,   KC_1,     KC_2,     KC_3,    KC_4,     KC_5,           KC_HOME,          KC_END,          KC_6,           KC_7,        KC_8,     KC_9,     KC_0,     KC_EQL,    \
-			KC_TAB,   KC_QUOT,  KC_COMM,  KC_DOT,  KC_P,     KC_Y,           KC_BSPC,          KC_DEL,          KC_F,           KC_G,        KC_C,     KC_R,     KC_L,     KC_BSLS,   \
-			KC_ESC,   KC_A,     KC_O,     KC_E,    KC_U,     KC_I,           CTL_T(KC_ENT),    CTL_T(KC_ENT),   KC_D,           KC_H,        KC_T,     KC_N,     KC_S,     KC_MINS,   \
-			KC_LSFT,  KC_SCLN,  KC_Q,     KC_J,    KC_K,     KC_X,           ALT_T(KC_PGUP),   ALT_T(KC_PGDN),  KC_B,           KC_M,        KC_W,     KC_V,     KC_Z,     KC_RSFT,   \
-			KC_CAPS,  KC_LEFT,  KC_DOWN,  KC_UP,   KC_RGHT,  GUI_T(KC_SPC),  MO(LAYER_RIGHT),  MO(LAYER_LEFT),  GUI_T(KC_SPC),  RCTL(KC_A),  KC_HYPR,  KC_LBRC,  KC_RBRC,  KC_SLSH \
+			KC_GRV,   KC_1,     KC_2,     KC_3,    KC_4,     KC_5,            KC_HOME,          KC_END,           KC_6,            KC_7,        KC_8,     KC_9,     KC_0,     KC_EQL,    \
+			KC_TAB,   KC_QUOT,  KC_COMM,  KC_DOT,  KC_P,     KC_Y,            KC_BSPC,          KC_DEL,           KC_F,            KC_G,        KC_C,     KC_R,     KC_L,     KC_BSLS,   \
+			KC_ESC,   KC_A,     KC_O,     KC_E,    KC_U,     KC_I,            LCTL_T(KC_ENT),   RCTL_T(KC_ENT),   KC_D,            KC_H,        KC_T,     KC_N,     KC_S,     KC_MINS,   \
+			KC_LSFT,  KC_SCLN,  KC_Q,     KC_J,    KC_K,     KC_X,            LALT_T(KC_PGUP),  RALT_T(KC_PGDN),  KC_B,            KC_M,        KC_W,     KC_V,     KC_Z,     KC_RSFT,   \
+			KC_CAPS,  KC_LEFT,  KC_DOWN,  KC_UP,   KC_RGHT,  RGUI_T(KC_SPC),  MO(LAYER_RIGHT),  MO(LAYER_LEFT),   RGUI_T(KC_SPC),  RCTL(KC_A),  KC_HYPR,  KC_LBRC,  KC_RBRC,  KC_SLSH \
 			),
 
 	/*
 	 * Left Momentary Layer: F-Keys
 	 */
 	[LAYER_LEFT] = LAYOUT( \
-			KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_TRNS,       TO(LAYER_SYS), KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   \
-			KC_TRNS,  KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_TRNS,       KC_TRNS,       KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   \
-			KC_TRNS,  KC_F11,   KC_F12,   KC_F13,   KC_F14,   KC_F15,   KC_TRNS,       KC_TRNS,       KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   \
-			KC_TRNS,  KC_F16,   KC_F17,   KC_F18,   KC_F19,   KC_F20,   KC_TRNS,       KC_TRNS,       KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   \
-			KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  TO(LAYER_QG),  KC_TRNS,       KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS \
+			xxxx,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   xxxx,            TO(LAYER_SYSTEM),  ____,  ____,  ____,  ____,  ____,  RGB_TOG,  \
+			xxxx,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  xxxx,            ____,              ____,  ____,  ____,  ____,  ____,  BL_MODE,  \
+			xxxx,  KC_F11,  KC_F12,  KC_F13,  KC_F14,  KC_F15,  xxxx,            ____,              ____,  ____,  ____,  ____,  ____,  RGB_VAI,  \
+			xxxx,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  xxxx,            ____,              ____,  ____,  ____,  ____,  ____,  RGB_VAD,  \
+			xxxx,  KC_F21,  KC_F22,  KC_F23,  KC_F24,  ____,    TO(LAYER_PADS),  ____,              ____,  ____,  ____,  ____,  ____,  ____ \
 			),
 
 	/*
 	 * Right Momentary: Media, Mouse, and RGB Controls
 	 */
 	[LAYER_RIGHT] = LAYOUT( \
-			KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  TO(LAYER_SYS),  KC_TRNS,       KC_PAUSE,  KC_PSCR,        KC_WH_U,     KC_SLCK,         BL_MODE,     RGB_TOG,   \
-			KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,        KC_TRNS,       KC_VOLU,   LALT(KC_LEFT),  KC_MS_UP,    LALT(KC_RIGHT),  KC_MS_BTN2,  RGB_VAI,   \
-			KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,        KC_TRNS,       KC_MUTE,   KC_MS_LEFT,     KC_MS_DOWN,  KC_MS_RIGHT,     KC_MS_BTN1,  RGB_VAD,   \
-			KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,        KC_TRNS,       KC_VOLD,   KC_WH_L,        KC_WH_D,     KC_WH_R,         RGB_HUI,     RGB_SAI,   \
-			KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,        TO(LAYER_QG),  KC_TRNS,   KC_PSCR,        KC_HYPR,     KC_SYSREQ,       RGB_HUD,     RGB_SAD \
+			____,  ____,  ____,  ____,  ____,  ____,  TO(LAYER_SYSTEM),  ____,            xxxx,     xxxx,        KC_WH_U,     xxxx,         xxxx,        xxxx,        \
+			____,  ____,  ____,  ____,  ____,  ____,  ____,              ____,            KC_VOLU,  KC_WH_L,     KC_MS_UP,    KC_WH_R,      xxxx,        xxxx,        \
+			____,  ____,  ____,  ____,  ____,  ____,  ____,              ____,            KC_MUTE,  KC_MS_LEFT,  KC_MS_DOWN,  KC_MS_RIGHT,  KC_MS_BTN1,  KC_MS_BTN2,  \
+			____,  ____,  ____,  ____,  ____,  ____,  ____,              ____,            KC_VOLD,  xxxx,        KC_WH_D,     xxxx,         KC_MS_BTN3,  KC_MS_BTN4,  \
+			____,  ____,  ____,  ____,  ____,  ____,  ____,              TO(LAYER_PADS),  ____,     ____,        ____,        xxxx,         KC_MS_BTN5,  xxxx \
 			),
 
 	/*
 	 * QWERTY Gaming
 	 */
-	[LAYER_QG] = LAYOUT( \
-			KC_GRV,    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,           KC_TRNS,         KC_TRNS,         KC_NO,          KC_NO,  KC_PSLS,   KC_PAST,  KC_PMNS,  KC_NO,    \
-			KC_TAB,    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,           KC_TRNS,         KC_TRNS,         KC_NO,          KC_P7,  KC_P8,     KC_P9,    KC_PPLS,  KC_NO,    \
-			KC_CAPS,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,           KC_TRNS,         KC_TRNS,         KC_TAB,         KC_P4,  KC_P5,     KC_P6,    KC_PENT,  KC_NO,    \
-			MOD_LSFT,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,           KC_TRNS,         KC_TRNS,         KC_NO,          KC_P1,  KC_P2,     KC_P3,    KC_PENT,  KC_NO,    \
-			KC_LALT,   KC_F1,  KC_F2,  KC_F3,  KC_F4,  GUI_T(KC_SPC),  TO(LAYER_HOME),  TO(LAYER_HOME),  GUI_T(KC_SPC),  KC_P0,  DBL_ZERO,  KC_PDOT,  KC_PCMM,  KC_END \
+	[LAYER_PADS] = LAYOUT( \
+			KC_GRV,    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,            ____,            ____,            xxxx,           xxxx,   KC_PSLS,   KC_PAST,  KC_PMNS,  xxxx,     \
+			KC_TAB,    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,            ____,            ____,            xxxx,           KC_P7,  KC_P8,     KC_P9,    KC_PPLS,  xxxx,     \
+			KC_CAPS,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,            ____,            ____,            KC_TAB,         KC_P4,  KC_P5,     KC_P6,    KC_PENT,  xxxx,     \
+			MOD_LSFT,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,            ____,            ____,            xxxx,           KC_P1,  KC_P2,     KC_P3,    KC_PENT,  xxxx,     \
+			KC_LALT,   KC_F1,  KC_F2,  KC_F3,  KC_F4,  RGUI_T(KC_SPC),  TO(LAYER_HOME),  TO(LAYER_HOME),  GUI_T(KC_SPC),  KC_P0,  DBL_ZERO,  KC_PDOT,  KC_PCMM,  KC_END \
 			),
+
 	/*
 	 * System: QMK Functions, Debug, & etc
 	 */
-	[LAYER_SYS] = LAYOUT( \
-			KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  RESET,           RESET,           KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    \
-			KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  EEPROM_RESET,    EEPROM_RESET,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    \
-			KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  DEBUG,           DEBUG,           KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    \
-			KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,           KC_NO,           KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    \
-			KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  TO(LAYER_HOME),  TO(LAYER_HOME),  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO  \
+	[LAYER_SYSTEM] = LAYOUT( \
+			xxxx,  xxxx,  xxxx,  xxxx,  xxxx,  xxxx,  RESET,           RESET,           xxxx,  xxxx,  xxxx,  xxxx,  xxxx,  xxxx,    \
+			xxxx,  xxxx,  xxxx,  xxxx,  xxxx,  xxxx,  EEPROM_RESET,    EEPROM_RESET,    xxxx,  xxxx,  xxxx,  xxxx,  xxxx,  xxxx,    \
+			xxxx,  xxxx,  xxxx,  xxxx,  xxxx,  xxxx,  xxxx,            xxxx,            xxxx,  xxxx,  xxxx,  xxxx,  xxxx,  xxxx,    \
+			xxxx,  xxxx,  xxxx,  xxxx,  xxxx,  xxxx,  xxxx,            xxxx,            xxxx,  xxxx,  xxxx,  xxxx,  xxxx,  xxxx,    \
+			xxxx,  xxxx,  xxxx,  xxxx,  xxxx,  xxxx,  TO(LAYER_HOME),  TO(LAYER_HOME),  xxxx,  xxxx,  xxxx,  xxxx,  xxxx,  xxxx  \
 			)
 				  
 };
-
-
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 	
