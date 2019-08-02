fe4d8585254ac4e34d60004b7df0efd8cab31f5d
Mike Hix <mhix@newrelic.com>
2019-08-02 11:59:46 -0700

    Move hyper, ctrl-a, layer shift keys a bit based on frequency of use

--- keyboards/hotdox/keymaps/musl/keymap.c
+++ keyboards/hotdox/keymaps/musl/keymap.c
@@ -1,4 +1,4 @@
-/* vim: set sw=2 ts=2 : */
+/* vim: set sw=3 ts=2 : */
 
 #include QMK_KEYBOARD_H
 
@@ -13,18 +13,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 			LALT_T(KC_PGUP), KC_NO,
 			KC_NO,
 			LGUI_T(KC_SPC), MO(1), // Thumbs
-			KC_NO,
+			KC_HYPR,
 
-			KC_END,          KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_EQL,
-			KC_DEL,          KC_F,     KC_G,     KC_C,     KC_R,     KC_L,     KC_BSLS,
-			KC_D,            KC_H,     KC_T,     KC_N,     KC_S,     KC_MINS,
-			RCTL_T(KC_ENT),  KC_B,     KC_M,     KC_W,     KC_V,     KC_Z,     KC_RSFT,
-			LCTL(KC_A),      KC_HYPR,  KC_LBRC,  KC_RBRC,  KC_SLSH,
+			KC_END,          KC_6,   KC_7,     KC_8,     KC_9,     KC_0,     KC_EQL,
+			KC_DEL,          KC_F,   KC_G,     KC_C,     KC_R,     KC_L,     KC_BSLS,
+			KC_D,            KC_H,   KC_T,     KC_N,     KC_S,     KC_MINS,
+			RCTL_T(KC_ENT),  KC_B,   KC_M,     KC_W,     KC_V,     KC_Z,     KC_RSFT,
+			MO(2),           MO(1),  KC_LBRC,  KC_RBRC,  KC_SLSH,
 			
 			KC_NO, RALT_T(KC_PGDN),
 			KC_NO,
-			RGUI_T(KC_SPC), MO(2), // Thumbs
-			KC_NO
+			KC_HYPR,
+			LCTL(KC_A), RGUI_T(KC_SPC)// Thumbs
 			),
 	[1] = LAYOUT_ergodox(
 			KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  TO(4),
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 			KC_NO, KC_NO,
 			KC_NO,
 			KC_NO, KC_TRNS, // Thumbs
-			KC_NO,
+			KC_HYPR,
 			
 			KC_NO,  KC_NO,    KC_NO,    KC_WH_U,  KC_NO,    KC_NO,    KC_NO,
 			KC_NO,  KC_VOLU,  KC_WH_L,  KC_MS_U,  KC_WH_R,  KC_NO,
@@ -46,8 +46,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 			
 			KC_NO, KC_NO,
 			KC_NO,
-			KC_NO, TO(3), // Thumbs
-			KC_NO
+			KC_HYPR,
+			TO(3), RGUI_T(KC_SPC)// Thumbs
 			),
 	[2] = LAYOUT_ergodox(
 			KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_NO,
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 			KC_NO, KC_NO,
 			KC_NO,
 			KC_NO, TO(3), // Thumbs 
-			KC_NO,
+			KC_HYPR,
 
 			TO(4),  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  BL_TOGG,
 			KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
@@ -69,8 +69,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 			
 			KC_NO, KC_NO,
 			KC_NO,
-			KC_NO, KC_TRNS, // Thumbs
-			KC_NO
+			KC_HYPR,
+			TO(3), RGUI_T(KC_SPC)// Thumbs
 			),
 	[3] = LAYOUT_ergodox(
 			KC_GESC,  KC_1,   KC_2,   KC_3,   KC_4,   KC_5,    KC_HOME,
@@ -92,8 +92,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 			
 			KC_NO, LALT_T(KC_PGDN),
 			KC_NO,
-			RGUI_T(KC_SPC), TO(0), // Thumbs
-			KC_HYPR
+			KC_HYPR,
+			TO(0), RGUI_T(KC_SPC)// Thumbs
 			),
 	[4] = LAYOUT_ergodox(
 			KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_TRNS,
@@ -105,7 +105,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 			KC_NO, KC_NO,
 			KC_NO,
 			KC_NO, TO(0), // Thumbs
-			KC_NO,
+			KC_HYPR,
 
 			KC_TRNS,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
 			RESET,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
@@ -115,7 +115,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 			KC_NO, KC_NO,
 			KC_NO,
-			KC_NO, TO(0), // Thumbs
-			KC_NO
+			KC_HYPR,
+			TO(0), RGUI_T(KC_SPC)// Thumbs
 			)
 };
