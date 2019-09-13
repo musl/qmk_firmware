fb3aa0de32c0286be5b9f99816b1a44389de5309
Mike Hix <mhix@newrelic.com>
2019-09-12 17:07:11 -0700

    Fix number key typo in my layout

--- keyboards/keebio/iris/keymaps/musl/keymap.c
+++ keyboards/keebio/iris/keymaps/musl/keymap.c
@@ -2,7 +2,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT(
-			KC_GRV,          KC_2,             KC_2,            KC_3,        KC_4,        KC_5,             KC_6,  KC_7,        KC_8,        KC_9,  KC_0,  KC_EQL,
+			KC_GRV,          KC_1,             KC_2,            KC_3,        KC_4,        KC_5,             KC_6,  KC_7,        KC_8,        KC_9,  KC_0,  KC_EQL,
 			KC_TAB,          KC_QUOT,          KC_COMM,         KC_DOT,      KC_P,        KC_Y,             KC_F,  KC_G,        KC_C,        KC_R,  KC_L,  KC_BSLS,
 			KC_ESC,          KC_A,             KC_O,            LT(1,KC_E),  LT(2,KC_U),  KC_I,             KC_D,  LT(3,KC_H),  LT(4,KC_T),  KC_N,  KC_S,  KC_MINS,
 			KC_LSFT,         KC_SCLN,          KC_Q,            KC_J,        KC_K,        KC_X,
