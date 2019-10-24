01da1d3b3de70f9a5f8ddbba73de647ab90eff97
Mike Hix <mhix@newrelic.com>
2019-10-24 13:07:24 -0700

    Add the Chiron keyboard

--- keyboards/handwired/chiron/keymaps/default/config.h
+++ keyboards/handwired/chiron/keymaps/default/config.h
@@ -22,6 +22,10 @@
 
 #define IGNORE_MOD_TAP_INTERRUPT
 #define PERMISSIVE_HOLD
+<<<<<<< HEAD
+=======
+#define PREVENT_STUCK_MODIFIERS
+>>>>>>> Add the Chiron keyboard
 
 #define TAPPING_TERM 200
 
--- keyboards/handwired/chiron/rules.mk
+++ keyboards/handwired/chiron/rules.mk
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # MCU name
 MCU = atmega32u4
 
@@ -14,24 +15,50 @@ BOOTLOADER = atmel-dfu
 # Build Options
 #   change yes to no to disable
 #
+=======
+# Required Items
+MCU = atmega32u4
+BOOTLOADER = atmel-dfu
+LAYOUTS = chiron
+SPLIT_KEYBOARD = yes
+
+# Stuff to turn off by default
+>>>>>>> Add the Chiron keyboard
 AUDIO_ENABLE = no
 AUTOLOG_ENABLE = no
 BACKLIGHT_ENABLE = no
 BLUETOOTH_ENABLE = no
+<<<<<<< HEAD
 BOOTMAGIC_ENABLE = no
+=======
+BOOT_MAGIC_ENABLE = no
+>>>>>>> Add the Chiron keyboard
 COMMAND_ENABLE = no
 CONSOLE_ENABLE = no
 DEBUG_ENABLE = no
 EXTRAKEY_ENABLE = no
 FAUXCLICKY_ENABLE = no
+<<<<<<< HEAD
 LEADER_ENABLE = no
 MIDI_ENABLE = no
 MOUSEKEY_ENABLE = yes
 NKRO_ENABLE = no
 RGBLIGHT_ENABLE = yes
 SLEEP_LED_ENABLE = yes
+=======
+HD44780_ENABLE = no
+LEADER_ENABLE = no
+MIDI_ENABLE = no
+MOUSEKEY_ENABLE = no
+NKRO_ENABLE = no
+RGBLIGHT_ENABLE = no
+SLEEP_LED_ENABLE = no
+>>>>>>> Add the Chiron keyboard
 TAP_DANCE_ENABLE = no
 UCIS_ENABLE = no
 UNICODEMAP_ENABLE = no
 UNICODE_ENABLE = no
+<<<<<<< HEAD
 SPLIT_KEYBOARD = yes
+=======
+>>>>>>> Add the Chiron keyboard
