29c00a266e2446e841221fc8dd22bd6eb756a7f4
Mike Hix <mhix@newrelic.com>
2019-11-26 18:56:16 -0800

    Remove tap-hold for space/os, just space please

--- keyboards/keebio/nyquist/keymaps/gamepad/keymap.c
+++ keyboards/keebio/nyquist/keymaps/gamepad/keymap.c
@@ -4,11 +4,11 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT(
-			KC_GRV,    KC_1,     KC_2,      KC_3,     KC_4,           KC_5,   XX,  XX,  XX,  XX,  XX,  XX,
-			KC_TAB,    KC_Q,     KC_W,      KC_E,     KC_R,           KC_T,   XX,  XX,  XX,  XX,  XX,  XX,
-			KC_ESC,    KC_A,     KC_S,      KC_D,     KC_F,           KC_G,   XX,  XX,  XX,  XX,  XX,  XX,
-			KC_LSFT,   KC_Z,     KC_X,      KC_C,     KC_V,           KC_B,   XX,  XX,  XX,  XX,  XX,  XX,
-			KC_ENTER,  KC_BSPC,  KC_LCTRL,  KC_LALT,  GUI_T(KC_SPC),  MO(1),  XX,  XX,  XX,  XX,  XX,  XX
+			KC_GRV,    KC_1,     KC_2,      KC_3,     KC_4,    KC_5,   XX,  XX,  XX,  XX,  XX,  XX,
+			KC_TAB,    KC_Q,     KC_W,      KC_E,     KC_R,    KC_T,   XX,  XX,  XX,  XX,  XX,  XX,
+			KC_ESC,    KC_A,     KC_S,      KC_D,     KC_F,    KC_G,   XX,  XX,  XX,  XX,  XX,  XX,
+			KC_LSFT,   KC_Z,     KC_X,      KC_C,     KC_V,    KC_B,   XX,  XX,  XX,  XX,  XX,  XX,
+			KC_ENTER,  KC_BSPC,  KC_LCTRL,  KC_LALT,  KC_SPC,  MO(1),  XX,  XX,  XX,  XX,  XX,  XX
 			),
 	[1] = LAYOUT(
 			RGB_MOD,  KC_NLCK,  KC_PSLS,  KC_PAST,  KC_PMNS,  KC_VOLU,  XX,  XX,  XX,  XX,  XX,  XX,
