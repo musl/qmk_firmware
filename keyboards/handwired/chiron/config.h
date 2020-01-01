7637ce427b894c8dd758e2d37f9ae03224f18a62
Mike Hix <mhix@newrelic.com>
2019-12-31 19:16:27 -0800

    Fix pin collision, remove unnecessary config

--- keyboards/handwired/chiron/config.h
+++ keyboards/handwired/chiron/config.h
@@ -48,6 +48,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGB_DI_PIN D3
 #define RGBLED_NUM 4
 
-#define BACKLIGHT_PIN B6
+#define BACKLIGHT_PIN D2
 #define BACKLIGHT_LEVELS 7
 
